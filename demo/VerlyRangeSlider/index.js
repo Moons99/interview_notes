f4553c2a0842d94468ef70566bac122841e1ffdf
HYY <885543243@qq.com>
2022-01-17 10:57:15 +0800

    feat: 物理引擎的滑块demo

---
+++ demo/VerlyRangeSlider/index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    VerlyRange('range-slider', '#655ecf');
+    VerlyRange('range-slider2', '#eb3992');
+    VerlyRange('range-slider3', '#14bd3e');
+    VerlyRange('range-slider4', '#395aeb');
+}
